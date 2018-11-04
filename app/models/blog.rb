ac123239d2cbed378747676e4f219adb5d456555
Vikas <d.vikas5b6@gmail.com>
2018-11-04 03:02:26 -0500

    refactor strong params for portfolio items

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,5 +3,5 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   validates_presence_of :title, :body
-  belongs_to :topic
+  belongs_to :topic, optional: true
 end
